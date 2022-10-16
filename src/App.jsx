87e4cfc096e2127a391a9ca34ac338032fed5954
edu <eduardomachucamoreno@gmail.com>
2022-10-16 15:13:09 +0200

    Calculating letter of spanish ID

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [id, setId] = useState()
+  const [letter, setLetter] = useState()
+  const arrayDNI = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"]
+  const calculateletter = () => {
+    const indexofarray = id % 23
+    setLetter(arrayDNI[indexofarray])
+    console.log(indexofarray)
 
+  }
+  
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input value={id} onChange={e => setId(e.target.value)} />
+      <button onClick={() => calculateletter()}> Calcular </button>
+      <div> {letter} </div>
     </div>
   )
 }
